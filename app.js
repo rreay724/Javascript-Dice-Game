c16e5dc976892374582901fd170c4bace1d1d047
bobbyreay <rreay724@gmail.com>
2020-01-16 16:03:04 -0500

    initial commit. Project completed. Practice project for learning javascript

---
+++ app.js
@@ -0,0 +1,189 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+// DOM: Document Object Model
+// Structured representation of an HTML document
+// The DOM is used to connect webpages to scripts like Javascript
+// For each HTML box, there is an object in the DOM that we can access and ineract with
+
+var scores, roundScore, activePlayer;
+var gamePlaying = true;
+var firstRoll, secondRoll;
+var scoreEntered;
+
+// state variable tells us the condition of the system and we need it when we need to remember something or the state of something
+// in this case - is our game playing or is our game not playing
+
+initializeGame();
+
+var lastDice, lastDice1;
+
+// console.log(dice);
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+
+/* <em> creates italic text</em> */
+// document.querySelector("#current-" + activePlayer).innerHTML =
+//   "<em>" + dice + "</em>";
+
+/* this will print whatever is set in score-0 to the console */
+// var x = document.querySelector("#score-0").textContent;
+// console.log(x);
+
+// function btn() {
+//   // Do something here
+// }
+
+// document.querySelector(".btn-roll").addEventListener("click", btn);
+// or can write the function in the event listener, since we'll only be using the function here
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  if (gamePlaying) {
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+
+    //2. display the result
+    document.getElementById("dice-1").style.display = "block";
+    document.getElementById("dice-2").style.display = "block";
+    document.getElementById("dice-1").src = "dice-" + dice1 + ".png";
+    document.getElementById("dice-2").src = "dice-" + dice2 + ".png";
+
+    //3. Update the round score IF the rolled number was not a 1
+    if ((dice1 === 6 && lastDice === 6) || (dice2 === 6 && lastDice1 === 6)) {
+      // Player loses score
+      scores[activePlayer] = 0;
+      document.querySelector("#score-" + activePlayer).textContent = "0";
+      nextPlayer();
+    } else if (dice1 !== 1 && dice2 !== 1) {
+      //Add score
+      // same as roundscore = roundscore + dice;
+      roundScore += dice1 + dice2;
+
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      // Next player turn
+      // activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+      nextPlayer();
+    }
+    lastDice = dice1;
+    lastDice1 = dice2;
+  }
+});
+
+// document
+//   .querySelector("#winning-score")
+//   .addEventListener("keypress", function(e) {
+//     if (e.key === "Enter") {
+//       scoreEntered =
+//         document.getElementsById("winning-score").value + scoreEntered;
+//       var scoreEnteredAlert = alert("Winning score is " + scoreEntered);
+//       scoreEnteredAlert;
+//     }
+//   });
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  if (gamePlaying) {
+    // add current score to users global score
+    scores[activePlayer] += roundScore;
+
+    // update UI
+    document.querySelector("#score-" + activePlayer).textContent =
+      scores[activePlayer];
+
+    var input = document.querySelector(".final-score").value;
+
+    var winningScore;
+
+    // Undefined, 0, null or empty string are COERCED to false
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
+    // Check if player won the game
+    if (scores[activePlayer] >= winningScore) {
+      document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+      hideDice();
+
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
+  }
+});
+
+function nextPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  roundScore = 0;
+
+  document.getElementsByClassName("current-0").textContent = "0";
+  document.getElementsByClassName("current-1").textContent = "0";
+
+  // use query selector and period because we are referencing a class
+  // document.querySelector(".player-0-panel").classList.remove("active");
+  // document.querySelector(".player-1-panel").classList.add("active");
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  // Make dice image disappear when dice hits 1 and it's the next players turn
+  hideDice();
+}
+
+document.querySelector(".btn-new").addEventListener("click", function() {
+  var newGameConfirmation = window.confirm(
+    "Are you sure you want to start a new game?"
+  );
+  newGameConfirmation;
+  if (newGameConfirmation == true) {
+    initializeGame();
+  }
+});
+
+function initializeGame() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  /* Will make the dice image disappear */
+  hideDice();
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+
+  gamePlaying = true;
+}
+
+function hideDice() {
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
+}
